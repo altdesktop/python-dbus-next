bf9e6c57df9624dddef45b1fb39f9193088e6786
Tony Crisci <tony@dubstepdish.com>
2019-05-03 12:17:21 -0400

    document constants

--- dbus_next/constants.py
+++ dbus_next/constants.py
@@ -5,23 +5,23 @@ class BusType(Enum):
     """An enum that indicates a type of bus. On most systems, there are
     normally two different kinds of buses running.
     """
-    SESSION = 1 #: A bus for the current graphical user session.
-    SYSTEM = 2 #: A persistent bus for the whole machine.
+    SESSION = 1  #: A bus for the current graphical user session.
+    SYSTEM = 2  #: A persistent bus for the whole machine.
 
 
 class MessageType(Enum):
     """An enum that indicates a type of message."""
-    METHOD_CALL = 1 #: An outgoing method call.
-    METHOD_RETURN = 2 #: A return to a previously sent method call
-    ERROR = 3 #: A return to a method call that has failed
-    SIGNAL = 4 #: A broadcast signal to subscribed connections
+    METHOD_CALL = 1  #: An outgoing method call.
+    METHOD_RETURN = 2  #: A return to a previously sent method call
+    ERROR = 3  #: A return to a method call that has failed
+    SIGNAL = 4  #: A broadcast signal to subscribed connections
 
 
 class MessageFlag(IntFlag):
     """Flags that affect the behavior of sent and received messages
     """
     NONE = 0
-    NO_REPLY_EXPECTED = 1 #: The method call does not expect a method return.
+    NO_REPLY_EXPECTED = 1  #: The method call does not expect a method return.
     NO_AUTOSTART = 2
     ALLOW_INTERACTIVE_AUTHORIZATION = 4
 
@@ -30,18 +30,18 @@ class NameFlag(IntFlag):
     """A flag that affects the behavior of a name request.
     """
     NONE = 0
-    ALLOW_REPLACEMENT = 1 #: If another client requests this name, let them have it.
-    REPLACE_EXISTING = 2 #: If another client owns this name, try to take it.
-    DO_NOT_QUEUE = 4 #: Name requests normally queue and wait for the owner to release the name. Do not enter this queue.
+    ALLOW_REPLACEMENT = 1  #: If another client requests this name, let them have it.
+    REPLACE_EXISTING = 2  #: If another client owns this name, try to take it.
+    DO_NOT_QUEUE = 4  #: Name requests normally queue and wait for the owner to release the name. Do not enter this queue.
 
 
 class RequestNameReply(Enum):
     """An enum that describes the result of a name request.
     """
-    PRIMARY_OWNER = 1 #: The bus owns the name.
-    IN_QUEUE = 2 #: The bus is in a queue and may receive the name after it is relased by the primary owner.
-    EXISTS = 3 #: The name has an owner and NameFlag.DO_NOT_QUEUE was given.
-    ALREADY_OWNER = 4 #: The bus already owns the name.
+    PRIMARY_OWNER = 1  #: The bus owns the name.
+    IN_QUEUE = 2  #: The bus is in a queue and may receive the name after it is relased by the primary owner.
+    EXISTS = 3  #: The name has an owner and NameFlag.DO_NOT_QUEUE was given.
+    ALREADY_OWNER = 4  #: The bus already owns the name.
 
 
 class ReleaseNameReply(Enum):
@@ -56,9 +56,9 @@ class PropertyAccess(Enum):
     """An enum that describes whether a DBus property can be gotten or set with
     the ``org.freedesktop.DBus.Properties`` interface.
     """
-    READ = 'read' #: The property is readonly.
-    WRITE = 'write' #: The property is writeonly.
-    READWRITE = 'readwrite' #: The property can be read or written to.
+    READ = 'read'  #: The property is readonly.
+    WRITE = 'write'  #: The property is writeonly.
+    READWRITE = 'readwrite'  #: The property can be read or written to.
 
     def readable(self):
         """Get whether the property can be read.
@@ -81,11 +81,11 @@ class ArgDirection(Enum):
 class ErrorType(Enum):
     """An enum for the type of an error for a message reply.
 
-    :seealso: http://man7.org/linux/man-pages/man3/sd-bus-errors.3.html 
+    :seealso: http://man7.org/linux/man-pages/man3/sd-bus-errors.3.html
     """
-    SERVICE_ERROR = 'com.dubstepdish.dbus.next.ServiceError' #: A custom error to indicate an exported service threw an exception.
-    INTERNAL_ERROR = 'com.dubstepdish.dbus.next.InternalError' #: A custom error to indicate something went wrong with the library.
-    CLIENT_ERROR = 'com.dubstepdish.dbus.next.ClientError' #: A custom error to indicate something went wrong with the client.
+    SERVICE_ERROR = 'com.dubstepdish.dbus.next.ServiceError'  #: A custom error to indicate an exported service threw an exception.
+    INTERNAL_ERROR = 'com.dubstepdish.dbus.next.InternalError'  #: A custom error to indicate something went wrong with the library.
+    CLIENT_ERROR = 'com.dubstepdish.dbus.next.ClientError'  #: A custom error to indicate something went wrong with the client.
 
     FAILED = "org.freedesktop.DBus.Error.Failed"
     NO_MEMORY = "org.freedesktop.DBus.Error.NoMemory"
