d0587631007d27a90c8616bb58a3942016a68c41
Rafał Gajda <rafal.gajda@silvair.com>
2020-01-16 15:55:19 +0100

    Rework test for signal

--- test/service/test_signals.py
+++ test/service/test_signals.py
@@ -1,6 +1,9 @@
-from dbus_next.service import ServiceInterface, signal, SignalDisabledError
+from dbus_next.service import ServiceInterface, signal, SignalDisabledError, dbus_property
 from dbus_next.aio import MessageBus
 from dbus_next import Message, MessageType
+from dbus_next.constants import PropertyAccess
+from dbus_next.signature import Variant
+
 
 import pytest
 import asyncio
@@ -33,6 +36,45 @@ class ExampleInterface(ServiceInterface):
         assert type(self) is ExampleInterface
 
 
+class ExpectMessage:
+    def __init__(self, bus1, bus2, timeout=1):
+        self.future = asyncio.get_event_loop().create_future()
+        self.bus1 = bus1
+        self.bus2 = bus2
+        self.timeout = timeout
+        self.timeout_task = None
+
+    def message_handler(self, msg):
+        self.timeout_task.cancel()
+        self.bus2.remove_message_handler(self.message_handler)
+        self.future.set_result(msg)
+        return True
+
+    def timeout_cb(self):
+        self.bus2.remove_message_handler(self.message_handler)
+        self.future.set_result(TimeoutError())
+
+    async def __aenter__(self):
+        self.bus2.add_message_handler(self.message_handler)
+        self.timeout_task = asyncio.get_event_loop().call_later(self.timeout, self.timeout_cb)
+
+        return self.future
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+def assert_signal_ok(signal, interface_name, export_path, unique_name, member, signature, body):
+    assert not isinstance(signal, TimeoutError)
+    assert signal.message_type == MessageType.SIGNAL
+    assert signal.interface == interface_name
+    assert signal.path == export_path
+    assert signal.sender == unique_name
+    assert signal.member == member
+    assert signal.signature == signature
+    assert signal.body == body
+
+
 @pytest.mark.asyncio
 async def test_signals():
     bus1 = await MessageBus().connect()
@@ -50,42 +92,53 @@ async def test_signals():
                 signature='s',
                 body=[f'sender={bus1.unique_name}']))
 
-    async def wait_for_message():
-        # TODO timeout
-        future = asyncio.get_event_loop().create_future()
+    async with ExpectMessage(bus1, bus2) as expected_signal:
+        interface.signal_empty()
+        assert_signal_ok(
+            signal=await expected_signal,
+            interface_name=interface.name,
+            export_path=export_path,
+            unique_name=bus1.unique_name,
+            member='signal_empty',
+            signature='',
+            body=[]
+        )
 
-        def message_handler(signal):
-            if signal.sender == bus1.unique_name and signal.interface == interface.name:
-                bus1.remove_message_handler(message_handler)
-                future.set_result(signal)
+    async with ExpectMessage(bus1, bus2) as expected_signal:
+        interface.original_name()
+        assert_signal_ok(
+            signal=await expected_signal,
+            interface_name=interface.name,
+            export_path=export_path,
+            unique_name=bus1.unique_name,
+            member='renamed',
+            signature='',
+            body=[]
+        )
 
-        bus2.add_message_handler(message_handler)
-        return await future
+    async with ExpectMessage(bus1, bus2) as expected_signal:
+        interface.signal_simple()
+        assert_signal_ok(
+            signal=await expected_signal,
+            interface_name=interface.name,
+            export_path=export_path,
+            unique_name=bus1.unique_name,
+            member='signal_simple',
+            signature='s',
+            body=['hello']
+        )
 
-    def assert_signal_ok(signal, member, signature, body):
-        assert signal.message_type == MessageType.SIGNAL, signal.body[0]
-        assert signal.interface == interface.name
-        assert signal.path == export_path
-        assert signal.sender == bus1.unique_name
-        assert signal.member == member
-        assert signal.signature == signature
-        assert signal.body == body
-
-    interface.signal_empty()
-    signal = await wait_for_message()
-    assert_signal_ok(signal, 'signal_empty', '', [])
-
-    interface.original_name()
-    signal = await wait_for_message()
-    assert_signal_ok(signal, 'renamed', '', [])
-
-    interface.signal_simple()
-    signal = await wait_for_message()
-    assert_signal_ok(signal, 'signal_simple', 's', ['hello'])
-
-    interface.signal_multiple()
-    signal = await wait_for_message()
-    assert_signal_ok(signal, 'signal_multiple', 'ss', ['hello', 'world'])
+    async with ExpectMessage(bus1, bus2) as expected_signal:
+        interface.signal_multiple()
+        assert_signal_ok(
+            signal=await expected_signal,
+            interface_name=interface.name,
+            export_path=export_path,
+            unique_name=bus1.unique_name,
+            member='signal_multiple',
+            signature='ss',
+            body=['hello', 'world']
+        )
 
     with pytest.raises(SignalDisabledError):
         interface.signal_disabled()
