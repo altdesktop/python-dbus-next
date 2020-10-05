altdesktop/python-dbus-next

commit 7efeef4f8f719ddf8703d4bd5d7c2150c4f9f456

unmarshaller: reduce number of read system calls
We can know in advance the total length of the messages, use that length
to read it all at once and store it into the buffer. Subsequent read()
calls are only accessing the buffer.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

dbus_next/_private/unmarshaller.py
@@ -177,6 +177,8 @@ class Unmarshaller:
 
     def _unmarshall(self):
         self.offset = 0
+        self.read(16)  # read all preamble at once
+        self.offset -= 16
         self.endian = self.read_byte()
         if self.endian != LITTLE_ENDIAN and self.endian != BIG_ENDIAN:
             raise InvalidMessageError('Expecting endianness as the first byte')
@@ -190,6 +192,11 @@ class Unmarshaller:
 
         body_len = self.read_uint32()
         serial = self.read_uint32()
+
+        # read the header len and backtrack since it needs to be read again
+        header_len = self.read_uint32()
+        self.read(header_len + body_len)  # read all header and body at once
+        self.offset -= 4 + header_len + body_len
 
         header_fields = {HeaderField.UNIX_FDS.name: []}
         for field_struct in self.read_argument(SignatureTree('a(yv)').types[0]):

commit cdfbb688693d3c1f81fcf9c2da33564e29da83a3

unmarshaller: prefetch header alignment bytes
When the message header is not a multiple of 8 bytes long, the alignment
bytes will not be prefetched and an additional read() system call will
be made when reading the end of the body.

Add the padding bytes to the prefetch length.

Fixes: 7efeef4f8f71 ("unmarshaller: reduce number of read system calls")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

dbus_next/_private/unmarshaller.py
@@ -228,7 +228,8 @@ class Unmarshaller:
         serial = self.read_uint32()
 
         header_len = self.read_uint32()
-        self.read(header_len + body_len, prefetch=True)
+        msg_len = header_len + self._padding(header_len, 8) + body_len
+        self.read(msg_len, prefetch=True)
         # backtrack offset since header array length needs to be read again
         self.offset -= 4
 