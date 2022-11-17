altdesktop/python-dbus-next

commit 3282eed607cb7f9f64a3275d1f9155c0e65bd8f8

Improve unmarshall performance
This change reduces the overhead of unmarshalling with the
goal of having bleak scanners not overwhelming the system.

See related issue:
https://github.com/hbldh/bleak/issues/236#issuecomment-789055069

```
(speed_up_unmarsh) % python3 bench/unmarshall.py
Unmarshalling 1000000 bluetooth rssi messages took 14.397348250000505 seconds
(master) % python3 bench/unmarshall.py
Unmarshalling 1000000 bluetooth rssi messages took 47.7756206660124 seconds
```

dbus_next/_private/constants.py
@@ -16,3 +16,8 @@ class HeaderField(Enum):
     SENDER = 7
     SIGNATURE = 8
     UNIX_FDS = 9
+
+
+HEADER_NAME_MAP = {
+    field.value: field.name for field in HeaderField
+}

dbus_next/constants.py
@@ -17,6 +17,8 @@ class MessageType(Enum):
     SIGNAL = 4  #: A broadcast signal to subscribed connections
 
 
+MESSAGE_TYPE_MAP = {field.value: field for field in MessageType}
+
 class MessageFlag(IntFlag):
     """Flags that affect the behavior of sent and received messages
     """
@@ -25,6 +27,8 @@ class MessageFlag(IntFlag):
     NO_AUTOSTART = 2
     ALLOW_INTERACTIVE_AUTHORIZATION = 4
 
+
+MESSAGE_FLAG_MAP = {field.value: field for field in MessageFlag}
 
 class NameFlag(IntFlag):
     """A flag that affects the behavior of a name request.

bench/unmarshall.py
@@ -1,0 +1,22 @@
+import io
+import timeit
+
+from dbus_next._private.unmarshaller import Unmarshaller
+
+bluez_rssi_message = (
+    "6c04010134000000e25389019500000001016f00250000002f6f72672f626c75657a2f686369302f6465"
+    "765f30385f33415f46325f31455f32425f3631000000020173001f0000006f72672e667265656465736b"
+    "746f702e444275732e50726f7065727469657300030173001100000050726f706572746965734368616e"
+    "67656400000000000000080167000873617b73767d617300000007017300040000003a312e3400000000"
+    "110000006f72672e626c75657a2e446576696365310000000e0000000000000004000000525353490001"
+    "6e00a7ff000000000000"
+)
+
+
+def unmarhsall_bluez_rssi_message():
+    Unmarshaller(io.BytesIO(bytes.fromhex(bluez_rssi_message))).unmarshall()
+
+
+count = 1000000
+time = timeit.Timer(unmarhsall_bluez_rssi_message).timeit(count)
+print(f"Unmarshalling {count} bluetooth rssi messages took {time} seconds")

dbus_next/message.py
@@ -7,7 +7,12 @@ from .signature import SignatureTree, Variant
 
 from typing import List, Any
 
-
+REQUIRED_FIELDS = {
+    MessageType.METHOD_CALL: ('path', 'member'),
+    MessageType.SIGNAL: ('path', 'member', 'interface'),
+    MessageType.ERROR: ('error_name', 'reply_serial'),
+    MessageType.METHOD_RETURN: ('reply_serial',),
+}
 class Message:
     """A class for sending and receiving messages through the
     :class:`MessageBus <dbus_next.message_bus.BaseMessageBus>` with the
@@ -95,21 +100,12 @@ class Message:
         if self.error_name is not None:
             assert_interface_name_valid(self.error_name)
 
-        def require_fields(*fields):
-            for field in fields:
-                if not getattr(self, field):
-                    raise InvalidMessageError(f'missing required field: {field}')
-
-        if self.message_type == MessageType.METHOD_CALL:
-            require_fields('path', 'member')
-        elif self.message_type == MessageType.SIGNAL:
-            require_fields('path', 'member', 'interface')
-        elif self.message_type == MessageType.ERROR:
-            require_fields('error_name', 'reply_serial')
-        elif self.message_type == MessageType.METHOD_RETURN:
-            require_fields('reply_serial')
-        else:
+        required_fields = REQUIRED_FIELDS.get(self.message_type)
+        if not required_fields:
             raise InvalidMessageError(f'got unknown message type: {self.message_type}')
+        for field in required_fields:
+            if not getattr(self, field):
+                raise InvalidMessageError(f'missing required field: {field}')
 
     @staticmethod
     def new_error(msg: 'Message', error_name: str, error_text: str) -> 'Message':

dbus_next/signature.py
@@ -1,7 +1,9 @@
 from .validators import is_object_path_valid
 from .errors import InvalidSignatureError, SignatureBodyMismatchError
 
+from functools import lru_cache
 from typing import Any, List, Union
+
 
 
 class SignatureType:
@@ -20,9 +22,9 @@ class SignatureType:
     """
     _tokens = 'ybnqiuxtdsogavh({'
 
-    def __init__(self, token):
+    def __init__(self, token: str) -> None:
         self.token = token
-        self.children = []
+        self.children: List[SignatureType] = []
         self._signature = None
 
     def __eq__(self, other):
@@ -257,43 +259,32 @@ class SignatureType:
         """
         if body is None:
             raise SignatureBodyMismatchError('Cannot serialize Python type "None"')
-        elif self.token == 'y':
-            self._verify_byte(body)
-        elif self.token == 'b':
-            self._verify_boolean(body)
-        elif self.token == 'n':
-            self._verify_int16(body)
-        elif self.token == 'q':
-            self._verify_uint16(body)
-        elif self.token == 'i':
-            self._verify_int32(body)
-        elif self.token == 'u':
-            self._verify_uint32(body)
-        elif self.token == 'x':
-            self._verify_int64(body)
-        elif self.token == 't':
-            self._verify_uint64(body)
-        elif self.token == 'd':
-            self._verify_double(body)
-        elif self.token == 'h':
-            self._verify_unix_fd(body)
-        elif self.token == 'o':
-            self._verify_object_path(body)
-        elif self.token == 's':
-            self._verify_string(body)
-        elif self.token == 'g':
-            self._verify_signature(body)
-        elif self.token == 'a':
-            self._verify_array(body)
-        elif self.token == '(':
-            self._verify_struct(body)
-        elif self.token == 'v':
-            self._verify_variant(body)
+        validator = self.validators.get(self.token)
+        if validator:
+            validator(self, body)
         else:
             raise Exception(f'cannot verify type with token {self.token}')
 
         return True
 
+    validators = {
+        "y": _verify_byte,
+        "b": _verify_boolean,
+        "n": _verify_int16,
+        "q": _verify_uint16,
+        "i": _verify_int32,
+        "u": _verify_uint32,
+        "x": _verify_int64,
+        "t": _verify_uint64,
+        "d": _verify_double,
+        "h": _verify_uint32,
+        "o": _verify_string,
+        "s": _verify_string,
+        "g": _verify_signature,
+        "a": _verify_array,
+        "(": _verify_struct,
+        "v": _verify_variant,
+    }
 
 class SignatureTree:
     """A class that represents a signature as a tree structure for conveniently
@@ -311,19 +302,15 @@ class SignatureTree:
         :class:`InvalidSignatureError` if the given signature is not valid.
     """
 
-    _cache = {}
-
     @staticmethod
-    def _get(signature: str = ''):
-        if signature in SignatureTree._cache:
-            return SignatureTree._cache[signature]
-        SignatureTree._cache[signature] = SignatureTree(signature)
-        return SignatureTree._cache[signature]
+    @lru_cache(maxsize=None)
+    def _get(signature: str = '') -> "SignatureTree":
+        return SignatureTree(signature)
 
     def __init__(self, signature: str = ''):
         self.signature = signature
 
-        self.types = []
+        self.types: List[SignatureType] = []
 
         if len(signature) > 0xff:
             raise InvalidSignatureError('A signature must be less than 256 characters')
@@ -381,7 +368,7 @@ class Variant:
         :class:`InvalidSignatureError` if the signature is not valid.
         :class:`SignatureBodyMismatchError` if the signature does not match the body.
     """
-    def __init__(self, signature: Union[str, SignatureTree, SignatureType], value: Any):
+    def __init__(self, signature: Union[str, SignatureTree, SignatureType], value: Any, verify: bool = True):
         signature_str = ''
         signature_tree = None
         signature_type = None
@@ -397,12 +384,13 @@ class Variant:
             raise TypeError('signature must be a SignatureTree, SignatureType, or a string')
 
         if signature_tree:
-            if len(signature_tree.types) != 1:
+            if verify and len(signature_tree.types) != 1:
                 raise ValueError('variants must have a signature for a single complete type')
             signature_str = signature_tree.signature
             signature_type = signature_tree.types[0]
 
-        signature_type.verify(value)
+        if verify:
+            signature_type.verify(value)
 
         self.type = signature_type
         self.signature = signature_str

dbus_next/validators.py
@@ -1,5 +1,6 @@
 import re
 from .errors import InvalidBusNameError, InvalidObjectPathError, InvalidInterfaceNameError, InvalidMemberNameError
+from functools import lru_cache
 
 _bus_name_re = re.compile(r'^[A-Za-z_-][A-Za-z0-9_-]*$')
 _path_re = re.compile(r'^[A-Za-z0-9_]+$')
@@ -7,6 +8,7 @@ _element_re = re.compile(r'^[A-Za-z_][A-Za-z0-9_]*$')
 _member_re = re.compile(r'^[A-Za-z_][A-Za-z0-9_-]*$')
 
 
+@lru_cache(maxsize=32)
 def is_bus_name_valid(name: str) -> bool:
     """Whether this is a valid bus name.
 
@@ -41,6 +43,7 @@ def is_bus_name_valid(name: str) -> bool:
     return True
 
 
+@lru_cache(maxsize=512)
 def is_object_path_valid(path: str) -> bool:
     """Whether this is a valid object path.
 
@@ -71,6 +74,7 @@ def is_object_path_valid(path: str) -> bool:
     return True
 
 
+@lru_cache(maxsize=32)
 def is_interface_name_valid(name: str) -> bool:
     """Whether this is a valid interface name.
 
@@ -100,7 +104,7 @@ def is_interface_name_valid(name: str) -> bool:
 
     return True
 
-
+@lru_cache(maxsize=512)
 def is_member_name_valid(member: str) -> bool:
     """Whether this is a valid member name.
 

test/test_marshaller.py
@@ -1,9 +1,12 @@
+from typing import Any
 from dbus_next._private.unmarshaller import Unmarshaller
-from dbus_next import Message, Variant, SignatureTree
+from dbus_next import Message, Variant, SignatureTree, MessageType, MessageFlag
 
 import json
 import os
 import io
+
+import pytest
 
 
 def print_buf(buf):
@@ -19,6 +22,17 @@ def print_buf(buf):
 # these messages have been verified with another library
 table = json.load(open(os.path.dirname(__file__) + '/data/messages.json'))
 
+# these have unix_fds so we only test with unmarshalling
+bluez_table = json.load(open(os.path.dirname(__file__) + '/data/bluez_messages.json'))
+
+def json_to_message(message: dict[str, Any]) -> Message:
+    copy = dict(message)
+    if "message_type" in copy:
+        copy['message_type'] = MessageType(copy['message_type'])
+    if "flags" in copy:
+        copy['flags'] = MessageFlag(copy['flags'])
+
+    return Message(**copy)
 
 # variants are an object in the json
 def replace_variants(type_, item):
@@ -54,7 +68,7 @@ def json_dump(what):
 
 def test_marshalling_with_table():
     for item in table:
-        message = Message(**item['message'])
+        message = json_to_message(item['message'])
 
         body = []
         for i, type_ in enumerate(message.signature_tree.types):
@@ -77,8 +91,9 @@ def test_marshalling_with_table():
         assert buf == data
 
 
-def test_unmarshalling_with_table():
-    for item in table:
+@pytest.mark.parametrize('unmarshall_table', (table, bluez_table))
+def test_unmarshalling_with_table(unmarshall_table):
+    for item in unmarshall_table:
 
         stream = io.BytesIO(bytes.fromhex(item['data']))
         unmarshaller = Unmarshaller(stream)
@@ -89,7 +104,7 @@ def test_unmarshalling_with_table():
             print(json_dump(item['message']))
             raise e
 
-        message = Message(**item['message'])
+        message = json_to_message(item['message'])
 
         body = []
         for i, type_ in enumerate(message.signature_tree.types):

commit 408ff0389a0ab62df29b715cb51fa3c2ed413f0a

Use enum's constructor to resolve enum values
Rather than creating a map of {value: enum}, let's just use Foo(value)
to resolve to the respective enum value.

This fixes a regression with introspection introduced in
commit 3282eed "Improve unmarshall performance":

  File "dbus_next/_private/unmarshaller.py", line 247, in _read_header
    self.flag = MESSAGE_FLAG_MAP[buffer[2]]
                ~~~~~~~~~~~~~~~~^^^^^^^^^^^
  KeyError: 0

MESSAGE_FLAG_MAP is built like this:
    MESSAGE_FLAG_MAP = {field.value: field for field in MessageFlag}

But MessageFlag is a IntFlag, so the zero value (NONE) is missing from
the iterator:

  >>> [f for f in dbus_next.constants.MessageFlag]
  [<MessageFlag.NO_REPLY_EXPECTED: 1>, <MessageFlag.NO_AUTOSTART: 2>,
   <MessageFlag.ALLOW_INTERACTIVE_AUTHORIZATION: 4>]

Resolving the enum through the constructor fixes this. MESSAGE_TYPE_MAP
and HEADER_NAME_MAP are changed in solidarity.

Fixes #142

dbus_next/_private/constants.py
@@ -16,6 +16,3 @@ class HeaderField(Enum):
     SENDER = 7
     SIGNATURE = 8
     UNIX_FDS = 9
-
-
-HEADER_NAME_MAP = {field.value: field.name for field in HeaderField}

dbus_next/_private/unmarshaller.py
@@ -5,9 +5,8 @@ from .constants import (
     LITTLE_ENDIAN,
     BIG_ENDIAN,
     PROTOCOL_VERSION,
-    HEADER_NAME_MAP,
 )
-from ..constants import MessageType, MessageFlag, MESSAGE_FLAG_MAP, MESSAGE_TYPE_MAP
+from ..constants import MessageType, MessageFlag
 from ..signature import SignatureTree, SignatureType, Variant
 from ..errors import InvalidMessageError
 
@@ -233,7 +232,7 @@ class Unmarshaller:
             o = self.offset + 1
             self.offset += signature_len + 2  # one for the byte, one for the '\0'
             tree = SignatureTree._get(self.buf[o:o + signature_len].decode())
-            headers[HEADER_NAME_MAP[field_0]] = self.read_argument(tree.types[0])
+            headers[HeaderField(field_0).name] = self.read_argument(tree.types[0])
         return headers
 
     def _read_header(self):
@@ -243,8 +242,8 @@ class Unmarshaller:
         self.read_to_offset(HEADER_SIGNATURE_SIZE)
         buffer = self.buf
         endian = buffer[0]
-        self.message_type = MESSAGE_TYPE_MAP[buffer[1]]
-        self.flag = MESSAGE_FLAG_MAP[buffer[2]]
+        self.message_type = MessageType(buffer[1])
+        self.flag = MessageFlag(buffer[2])
         protocol_version = buffer[3]
 
         if endian != LITTLE_ENDIAN and endian != BIG_ENDIAN:

dbus_next/constants.py
@@ -17,9 +17,6 @@ class MessageType(Enum):
     SIGNAL = 4  #: A broadcast signal to subscribed connections
 
 
-MESSAGE_TYPE_MAP = {field.value: field for field in MessageType}
-
-
 class MessageFlag(IntFlag):
     """Flags that affect the behavior of sent and received messages
     """
@@ -27,9 +24,6 @@ class MessageFlag(IntFlag):
     NO_REPLY_EXPECTED = 1  #: The method call does not expect a method return.
     NO_AUTOSTART = 2
     ALLOW_INTERACTIVE_AUTHORIZATION = 4
-
-
-MESSAGE_FLAG_MAP = {field.value: field for field in MessageFlag}
 
 
 class NameFlag(IntFlag):